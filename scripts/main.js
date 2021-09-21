57ab79a85ea020c06bbdf16a91a177589e49c23d
MattSux <m_heilman@fhsu.edu>
2021-09-21 14:03:46 -0500

    finished proj 04

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+//#1
+let myNumber;
+//#2
+myNumber=5;
+//#3
+let myOtherNumber = 5;
+//#4
+let myTotal = myNumber*myOtherNumber;
+//#5
+let myName = "";
+let myCombo = myNumber+myOtherNumber+"";
+//#6
+let myOtherCombo = myNumber+myOtherNumber;
+//#7
+let myRemainder = myNumber % 5;
+//#8
+let i = 2;
+//#9
+let int = 4;
+//#10
+let myBoolean = false;
